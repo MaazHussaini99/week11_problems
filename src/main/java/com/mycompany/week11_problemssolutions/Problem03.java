81b7134abc01e23db5412b55bc830ac8630ac751
MaazHussaini99 <maazhussaini@outlook.com>
2021-11-19 23:35:14 -0500

    Problems solved

--- src/main/java/com/mycompany/week11_problemssolutions/Problem03.java
+++ src/main/java/com/mycompany/week11_problemssolutions/Problem03.java
@@ -4,6 +4,10 @@
  */
 package com.mycompany.week11_problemssolutions;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * Solve problem 03:
@@ -22,6 +26,71 @@ Find the number of entries which are not divisible by 7 in the first one billion
 * 
  */
 public class Problem03 {
-
     
+    
+    public static List<List<BigInteger>> generate(int numRows){
+        
+        List<List<BigInteger>> triangle = new ArrayList<>();
+        
+        if (numRows == 0) return triangle;
+        
+        List<BigInteger> first_row = new ArrayList<>();
+        first_row.add(BigInteger.valueOf(1));
+        triangle.add(first_row);
+        
+        for (int i = 1; i < numRows; i++) {
+            List<BigInteger> prev_row = triangle.get(i-1);
+            List< BigInteger> row = new ArrayList<>();
+            
+            row.add(BigInteger.valueOf(1));
+            
+            for (int j = 1; j < i; j++) {
+                row.add(prev_row.get(j-1).add(prev_row.get(j)));
+            }
+            row.add(BigInteger.valueOf(1));
+            triangle.add(row);
+        }
+        
+        return triangle;
     }
+    
+    static boolean isDivisibleBy7(Long num)
+    {
+        // If number is negative, make it positive
+        if( num < 0 )
+            return isDivisibleBy7( -num );
+  
+        // Base cases
+        if( num == 0 || num == 7 )
+            return true;
+        if( num < 10 )
+            return false;
+  
+        // Recur for ( num / 10 - 2 * num % 10 )
+        return isDivisibleBy7( num / 10 - 2 * ( num - num / 10 * 10 ) );
+    }
+    
+    public static void main(String[] args) {
+        
+        List<List<BigInteger>> triangle = new ArrayList<>();
+        
+        triangle = generate(101);
+        long counter = 0;
+        long counter1 = 0;
+        
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("    ");
+                System.out.print(triangle.get(i).get(j));
+                counter1++;
+                if (isDivisibleBy7((triangle.get(i).get(j)).longValue())) {
+                    counter+=1;
+                }
+            }
+           
+            System.out.println("    ");
+        }
+        System.out.println("Counter of Number's not divisible by 7: " + counter);
+        System.out.println("Total numbers: " + counter1);
+    }
+}
